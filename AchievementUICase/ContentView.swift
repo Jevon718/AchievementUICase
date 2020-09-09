ac9bdd2958dc222fc666f50c6cb9bad99a234cb7
Jevon Charles <JevonCharlesbuild@gmail.com>
2020-09-09 11:46:52 -0400

    Initial Commit

---
+++ AchievementUICase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AchievementUICase
+//
+//  Created by Jevon Charles on 9/9/20.
+//  Copyright © 2020 Jevon Charles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
